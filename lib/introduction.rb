756d795196b83659c621064b4333481e55cbd8a1
Spencer Cambern <cambern134@gmail.com>
2020-04-19 08:49:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
    puts="Hi,my name is #{name}.\n"
 end
 
- def introduction_with_language (Dan, language)
+ def introduction_with_language (name, language)
   puts="Hi, my name is#{dan} and I am learning to program in #{language}.js."
  end
